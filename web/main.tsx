2d67491fc48ae4f2efa0d36fcac11d10f49e391f
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2026-02-13 13:27:03 +0000

    trigger build

--- web/main.tsx
+++ web/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 //import App from './App.tsx'
 import City from './City.tsx'
 
+// trigger application build
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <City />
