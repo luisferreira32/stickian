cb12f28e6006402db82762b8937c342fe80ea9e6
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2026-02-28 12:50:50 +0000

    documentation: Improve development process docs (#32)

--- server/const.go
+++ server/const.go
@@ -5,5 +5,5 @@ const (
 	defaultAddress       = "0.0.0.0:" + defaultServerPort
 	deafultMigrationsURL = "file://./migrations"
 	// YES, this is in plain text. But it is literally just in your local environment.
-	testDatabaseURL = "postgres://postgres:postgres@localhost:5432/dbname?sslmode=disable"
+	testDatabaseURL = "postgres://postgres:postgres@localhost:5432/app?sslmode=disable"
 )
