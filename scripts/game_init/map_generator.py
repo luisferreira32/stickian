d3a734e892686314ddd5e431d0d7ec5c22f4a637
antoniomf97 <antonio.mf97@gmail.com>
2026-03-12 16:05:57 +0000

    updated visuals

--- scripts/game_init/map_generator.py
+++ scripts/game_init/map_generator.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as pltc
 from scipy.stats import qmc
-
+from matplotlib.collections import PatchCollection
 
 def norm1_distance(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
@@ -17,7 +17,6 @@ class WorldGenerator:
     world_w = 256
     world_h = 256
     i_min = 10
-    i_max = 20
     isl_size = 7
     m_cities = 3
     max_mountain = 5
@@ -27,17 +26,14 @@ class WorldGenerator:
     max_coast = 3
 
     def __init__(self):
-        pass
+        self.world = [[0 for _ in range(self.world_w)] for _ in range(self.world_h)]
 
     def generate_map(self):
-        world = [[0 for _ in range(self.world_w)] for _ in range(self.world_h)]
-        centers = self.generate_island_centers()
-        for center in centers:
+        self.centers = self.generate_island_centers()
+        for center in self.centers:
             x, y = center
             print(x, y)
-            world[x][y] = 5
-
-        return world
+            self.world[x][y] = 5
 
     def generate_island_centers(self, k=30) -> list[tuple]:
         """
@@ -77,19 +73,24 @@ class WorldGenerator:
             "dimgrey",
         ]
 
+        fig, ax = plt.subplots()
+
         norm = plt.Normalize(min(cvals), max(cvals))
         tuples = list(zip(map(norm, cvals), colors))
         cmap = pltc.LinearSegmentedColormap.from_list("", tuples)
         cmap = pltc.ListedColormap(colors)
 
+        for xi, yi in self.centers:
+            ax.Circle((xi, yi), radius=self.i_min / max(self.world_w, self.world_h)/2, fill=False)
+
         # Plot with matplotlib
-        plt.imshow(self.world, cmap=cmap, vmin=0.5, vmax=5.5)
-        plt.colorbar()
-        plt.title("World Map")
+        ax.imshow(self.world, cmap=cmap, vmin=0.5, vmax=5.5)
+        ax.colorbar()
+        ax.title("World Map")
         plt.show()
 
     def run(self):
-        self.world = self.generate_map()
+        self.generate_map()
         self.show_map()
 
 
