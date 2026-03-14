de704d43c1ae559737a3effb6645c7027ff33f2d
antoniomf97 <antonio.mf97@gmail.com>
2026-03-14 11:17:57 +0000

    updated island creation

--- scripts/game_init/map_generator.py
+++ scripts/game_init/map_generator.py
@@ -16,33 +16,32 @@ def norm2_distance(a, b):
 
 
 class WorldGenerator:
-    world_w = 100
-    world_h = 100
-    border = 5
-    i_min = 15
-    i_size = 7
+    # world gen
+    world_w = 128
+    world_h = 128
+    border = 8
+
+    #island gen
+    i_min = 12
+    i_size = 4
     m_min = 3
-    m_max = 6
+    m_max = 8
     p_min = 9
-    p_max = 12
+    p_max = 14
     c_min = 15
-    c_max = 18
+    c_max = 20
 
     def __init__(self):
-        self.world = [[0 for _ in range(self.world_w)] for _ in range(self.world_h)]
+        self.world = np.zeros([])
+        self.islands = np.zeros([])
+        self.centers = np.zeros([])
 
     def generate_map(self):
-        self.centers = self.generate_island_centers(self.border)
-        for center in self.centers:
-            x, y = center
-            self.world[y][x] = 5
+        self.world = np.zeros((self.world_w,self.world_h))
+        self.islands = self.generate_islands()
+        print(self.islands[0])
 
-            land = self.generate_island_shape(center)
-            for l in land:
-                x, y = l
-                self.world[y][x] = 5
-
-    def generate_island_centers(self, margin, k=30) -> list[tuple]:
+    def generate_island_centers(self, k=30) -> list[tuple]:
         """
         Fast Poisson Disk Sampling in Arbitrary Dimensions
 
@@ -54,53 +53,68 @@ class WorldGenerator:
 
         rng = np.random.default_rng()
         engine = qmc.PoissonDisk(
-            d=2, radius=self.i_min / max(self.world_w-2*margin, self.world_h-2*margin), 
+            d=2, radius=self.i_min / max(self.world_w-2*self.border, self.world_h-2*self.border), 
             rng=rng, ncandidates=k
         )
         sample = engine.fill_space()
 
         centers = []
         for x, y in sample:
-            cx = int(x * self.world_w) + self.border
-            cy = int(y * self.world_h) + self.border
+            cx = int(x * (self.world_w-2*self.border)) + self.border
+            cy = int(y * (self.world_h-2*self.border)) + self.border
 
-            cx = self.world_w if cx > self.world_w else cx
-            cy = self.world_h if cy > self.world_h else cy
+            cx = self.world_w - 1 if cx >= self.world_w else cx
+            cy = self.world_h - 1 if cy >= self.world_h else cy
             centers.append((cx, cy))
 
         return centers
 
     def generate_island_shape(self, center):
         cx, cy = center
-        radius = 4 #random.randint(3,5)
+        radius = self.i_size
+
         valid_island = False
 
         while not valid_island:
             land = set()
             noise = OpenSimplex(seed=random.randint(0, 999999))
-            for x in range(max(0, cx-radius*2), min(self.world_w, cx+radius*2)):
-                for y in range(max(0, cy-radius*2), min(self.world_h, cy+radius*2)):
+            for x in range(max(0, cx-radius), min(self.world_w, cx+radius)):
+                for y in range(max(0, cy-radius), min(self.world_h, cy+radius)):
 
                     d = math.sqrt((x-cx)**2 + (y-cy)**2)
 
-                    n1 = noise.noise2(x*0.01, y*0.05)
-                    n2 = noise.noise2(x*0.1, y*0.15)
-                    # n3 = noise.noise2(x*0.25, y*0.25)
-                    nval = n1 + n2
+                    n1 = noise.noise2(x*0.01, y*0.01)
+                    n2 = noise.noise2(x*0.1, y*0.1)
+                    n3 = noise.noise2(x*0.3, y*0.3)
+                    nval = n1 #+ n2 + n3
 
                     threshold = radius * (1 + 0.4*nval)
 
                     if d < threshold:
                         land.add((x, y))
+                        
             min_size = self.m_min + self.p_min + self.c_min
             max_size = self.m_max + self.p_max + self.c_max
+
             if min_size <= len(land) <= max_size:
                 valid_island = True
             
         return land
 
     def generate_islands(self):
-        pass
+        islands = []
+        self.centers = self.generate_island_centers()
+        for center in self.centers:
+            x, y = center
+            self.world[y][x] = 5
+
+            land = self.generate_island_shape(center)
+            islands.append(land)
+            for l in land:
+                x, y = l
+                self.world[y][x] = 5
+        
+        return islands
 
     def show_map(self):
         cvals = [1, 2, 3, 4, 5]
@@ -119,19 +133,19 @@ class WorldGenerator:
 
         fig, ax = plt.subplots()
 
-        # plot island centers with minimum distance
-        for xi, yi in self.centers:
-            circle = plt.Circle((xi, yi), radius=self.i_min/2-1, fill=False)    
-            ax.add_artist(circle)
-
         # plot world map
         ax.imshow(self.world, cmap=cmap, vmin=0.5, vmax=5.5)
         ax.set_xlim(0, self.world_w-1)
         ax.set_ylim(0, self.world_h-1)
 
+        # plot island centers with minimum distance
+        for xi, yi in self.centers:
+            circle = plt.Circle((xi, yi), radius=self.i_min/2-1, fill=False)    
+            ax.add_artist(circle)
+
         # plot voronoi regions
         vor = Voronoi(self.centers)
-        voronoi_plot_2d(vor, ax=ax, show_vertices=False, line_colors='black')
+        voronoi_plot_2d(vor, ax=ax, show_vertices=False, line_colors='gray', line_style='-.-')
 
         # plt.colorbar()
         plt.title("World Map")
@@ -139,7 +153,6 @@ class WorldGenerator:
 
     def run(self):
         self.generate_map()
-        self.generate_islands()
         self.show_map()
 
 
